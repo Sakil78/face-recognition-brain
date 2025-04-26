c953f7faf713ee1115d3c7c5872ed2afdf52419a
Sakil Ahmed <ahmedsakil2014@hotmail.com>
2025-04-26 16:10:25 +0530

    Add particles background effect and implement ImageLinkForm and Rank components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
+      <ParticlesBg type="fountain" bg={true} /> {/* color, ball, lines, thick, circle, cobweb, polygon, square,tadpole, fountain, random, custom */}
+       
       <Navigation />
        <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+       <Rank />
+       <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
 }
-
 export default App;
